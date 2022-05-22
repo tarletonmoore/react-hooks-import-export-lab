f8a159f7d7a6e6c4c3784030c155bd8e12cc5f05
John Tarleton Moore <tarletonmoore@gmail.com>
2022-05-22 14:46:48 -0400

    commit tree b3b38993e82e7cb58e675d03d0f3368327af9395

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
+import * as Info from "../data/user"
 
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {Info.username} is a Web Developer from {Info.city}
       </h1>
     </div>
   );
 }
+
+export default Home
